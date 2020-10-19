8693a2932e03793215ff6b8db37e1e0a468c545c
Cesar Olivero <oliverogero@gmail.com>
2020-10-19 13:57:49 -0400

    delete

--- src/send-mail/server-mail.js
+++ src/send-mail/server-mail.js
@@ -1,11 +1,11 @@
 import nodemailer from "nodemailer";
 const serverMail = nodemailer.createTransport({
-  host: "email-smtp.us-east-1.amazonaws.com",
+  host: "",
   port: 587,
   secure: false,
   auth: {
-    user: "AKIAV27FGQZHT3NTD2E6",
-    pass: "BHPk5cpk+qJ00Sp8/trUxL3tYPjIXfrkOmneSwfKTIbH"
+    user: "]",
+    pass: ""
   }
 });
 
